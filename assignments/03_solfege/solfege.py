5f8eb9cfc8da09eacb92e318ddd54b8f67959087
natalierpayne <nataliermercer@gmail.com>
2021-09-20 13:22:15 -0700

    solfege

---
+++ assignments/03_solfege/solfege.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Author : nataliermercer <nataliermercer@localhost>
+Date   : 2021-09-20
+Purpose: Solfege
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='solfege',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('solfege',
+                        metavar='str', nargs='+',
+                        help='A positional argument')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    solfege_defs = {
+        'Do': 'A deer, a female deer',
+        'Re': 'A drop of golden sun',
+        'Mi': 'A name I call myself',
+        'Fa': 'A long long way to run',
+        'Sol': 'A needle pulling thread',
+        'La': 'A note to follow sol',
+        'Ti': 'A drink with jam and bread'
+    }
+
+    for char in args.solfege:
+        if char in solfege_defs:
+            print(char + ', ' + solfege_defs[char])
+        else:
+            print("I don't know " + '"' + char + '"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
