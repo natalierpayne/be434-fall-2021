b19a171079a2f6c9df2a15187a5a5dcde6c00ae2
natalierpayne <nataliermercer@gmail.com>
2021-09-28 22:40:16 -0700

    more passing tests

--- assignments/04_cat/cat.py
+++ assignments/04_cat/cat.py
@@ -21,7 +21,7 @@ def get_args():
                         metavar='FILE', 
                         type=argparse.FileType('rt'),
                         nargs='+',
-                        help='Input file(s)') #type? needed to fail w/o file. but makes input file unreadable?
+                        help='Input file(s)')
 
     parser.add_argument('-n',
                         '--number',
@@ -37,18 +37,30 @@ def main():
 
     args = get_args()
 
-    #infile = str(args.infile)
-
-    #print(args.infile)
-
-    if os.path.isfile(args.infile):
-        infile = open(args.infile).read().rstrip()
-    else:
-        print("[Errno 2] No such file or directory: " + os.path.basename(args.infile))
+    #if os.path.isfile(args.infile):
+    #    infile = open(args.infile).read().rstrip()
+    #else:
+    #    print("[Errno 2] No such file or directory: " + os.path.basename(args.infile))
 
     #for fh in args.infile:
+    #    for line in fh:
+    #        print(line)
 
-    #print(infile)
+    line_num = 0
+    if args.number:
+        for fh in args.infile:
+            for num, line in enumerate(fh, start=1):
+                print('     ' + str(num) + "\t" + line, end='')
+                line_num += 1
+    else:    
+        for fh in args.infile:
+            for line in fh:
+                print(line, end='')
+
+    #if args.number:
+    #    for fh in args.infile:
+    #        for num, line in enumerate(fh, start=1):
+    #            print(num, line)
 
 
 # --------------------------------------------------
